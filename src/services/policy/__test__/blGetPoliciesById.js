bb5c0ca7ca0237b210d43f98198cf0d900601b1d
Daniel <daniel.sola.fraire@gmail.com>
2019-03-23 11:09:58 +0100

    Added policies tests

---
+++ src/services/policy/__test__/blGetPoliciesById.js
@@ -0,0 +1,51 @@
+import test from 'tape';
+import { stub } from 'sinon';
+import proxyquire from 'proxyquire';
+import { PolicyNotFound } from 'errorcodes/policy';
+import CustomError from 'CustomError';
+
+const CompanyAPIServiceMock = {
+  '@noCallThru': true,
+   getClients: stub(),
+   getPolicies: stub(),
+};
+
+const existingPolicyId = 1234;
+const nonExistingPolicyId = 4321;
+
+const expectedPolicy = { name: 'john', id: existingPolicyId, clientId: 5432 };
+
+const policiesMock = { policies: [expectedPolicy] };
+
+const blGetPolicyById = proxyquire('../blGetPolicyById', {
+  '../CompanyAPIService': CompanyAPIServiceMock,
+}).default;
+
+test('blGetPolicyById', t => {
+  t.test('blGetPolicyById returns user policies', async ({ ok, deepEqual, end }) => {
+    CompanyAPIServiceMock.getPolicies.resolves(policiesMock);
+
+    const policies = await blGetPolicyById({policyId: existingPolicyId});
+
+    ok(CompanyAPIServiceMock.getPolicies.calledOnce, 'Should call CompanyServiceMock Get policies');
+    deepEqual(policies, expectedPolicy, 'Should return expected policy');
+
+    CompanyAPIServiceMock.getPolicies.reset();
+
+    end();
+  });
+
+  t.test('blGetPolicyById throws error if no policy found', async ({ ok, deepEqual, end }) => {
+    CompanyAPIServiceMock.getPolicies.resolves(policiesMock);
+    const policyNotFound = new CustomError(PolicyNotFound);
+
+    try {
+        await blGetPolicyById({policyId: nonExistingPolicyId});
+    } catch(error) {
+        ok(CompanyAPIServiceMock.getPolicies.calledOnce, 'Should call CompanyServiceMock Get clients');
+        deepEqual(error, policyNotFound,'Shoud throw PolicyNotFound error');
+    }
+
+    end();
+  });
+});
---
+++ src/services/policy/__test__/blGetPoliciesByName.js
@@ -0,0 +1,58 @@
+import test from 'tape';
+import { stub } from 'sinon';
+import proxyquire from 'proxyquire';
+import { UserNotFound } from 'errorcodes/user';
+import CustomError from 'CustomError';
+
+const CompanyAPIServiceMock = {
+  '@noCallThru': true,
+   getClients: stub(),
+   getPolicies: stub(),
+};
+
+const existingName = 'john';
+const nonExistingName = 'mark';
+
+const expectedUser = { name: existingName, id: 1234 };
+const clientsMock = {
+    clients: [expectedUser]
+};
+
+const expectedPolicy = [{ name: existingName, policyId: 4321, clientId: 1234 }];
+
+const policiesMock = { policies: expectedPolicy };
+
+const blGetPoliciesByName = proxyquire('../blGetPoliciesByName', {
+  '../CompanyAPIService': CompanyAPIServiceMock,
+}).default;
+
+test('blGetPoliciesByName', t => {
+  t.test('blGetPoliciesByName returns user policies', async ({ ok, deepEqual, end }) => {
+    CompanyAPIServiceMock.getClients.resolves(clientsMock);
+    CompanyAPIServiceMock.getPolicies.resolves(policiesMock);
+
+    const policies = await blGetPoliciesByName({userName: existingName});
+
+    ok(CompanyAPIServiceMock.getClients.calledOnce, 'Should call CompanyServiceMock Get clients');
+    ok(CompanyAPIServiceMock.getPolicies.calledOnce, 'Should call CompanyServiceMock Get policies');
+    deepEqual(policies, expectedPolicy, 'Should return expected policy');
+
+    CompanyAPIServiceMock.getClients.reset();
+
+    end();
+  });
+
+  t.test('blGetPoliciesByName throws error if user not found', async ({ ok, deepEqual, end }) => {
+    CompanyAPIServiceMock.getClients.resolves(clientsMock);
+    const userNotFoundError = new CustomError(UserNotFound);
+
+    try {
+        await blGetPoliciesByName({userName: nonExistingName});
+    } catch(error) {
+        ok(CompanyAPIServiceMock.getClients.calledOnce, 'Should call CompanyServiceMock Get clients');
+        deepEqual(error, userNotFoundError,'Shoud throw UserNotFound error');
+    }
+
+    end();
+  });
+});
